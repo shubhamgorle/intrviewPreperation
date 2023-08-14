81e19abe79444a6cd6498a64561310b7e10949d5
unknown <shubhamgorle.me@sbjit.edu.in>
2023-08-14 11:13:07 +0530

    reacttt

--- React/framework/src/App.js
+++ React/framework/src/App.js
@@ -9,50 +9,68 @@
 
 // 1. **************************************
 
+import {Profiler} from 'react'
+import './App.css';
+ import Count from './components/problem1/Count'
+import { useState } from 'react';
+import data1 from './db.json'
+function App() {
+ const [data,setdata] = useState(data1) 
+ const HandleQuantity = (count, id)=>{
+ var arr =  data.map((ele, idx)=>{
+     if(id === ele.id){
+      return {...ele, quantity : ele.quantity + count}
+     }
+     else{
+      return ele
+     }
+  })
+  setdata(arr)
+ }
+   var total = data.reduce((one, two)=>{
+          return one + two.price*two.quantity
+   },0)
+   
+   const onRenderCallback = ()=>{
+    console.log("rendering is completed")
+   }
 
-// import './App.css';
-// import Count from './components/problem1/Count';
-// import { useState } from 'react';
-// import data1 from './db.json'
-// function App() {
-//  const [data,setdata] = useState(data1) 
-//  const HandleQuantity = (count, id)=>{
-//  var arr =  data.map((ele, idx)=>{
-//      if(id === ele.id){
-//       return {...ele, quantity : ele.quantity + count}
-//      }
-//      else{
-//       return ele
-//      }
-//   })
-//   setdata(arr)
-//  }
-//    var total = data.reduce((one, two)=>{
-//           return one + two.price*two.quantity
-//    },0)
-//   return (
-//     <div className="App">
-//    {
-//     data.map((ele)=>
-//     <Count name = {ele.name} price = {ele.price} id = {ele.id}  quantity = {ele.quantity} HandleQuantity = {HandleQuantity}/>
-//     )
-//    }
-//    <h1>Total : {total}</h1>
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <Profiler id="MyAppProfiler" onRender={onRenderCallback}>
+      {
+    data.map((ele)=>
+    <Count name = {ele.name} price = {ele.price} id = {ele.id}  quantity = {ele.quantity} HandleQuantity = {HandleQuantity} key={ele.id}/>
+    )
+   }
+   <h1>Total : {total}</h1>
+            
+            </Profiler>
+   
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
 // 2. **************************************
 
-function App(){
-  return (
-        <div className="App">
-         
-        </div>
-      );
-}
-export default App;
+// import Profiler from 'react'
+// import Count from './components/problem1/Count.jsx'
+// function App(){
+//   const onRenderCallback = ()=>{
+//     console.log('rendered')
+//   }
+//   return (
+//         <div className="App">
+//            <h1>My Profiler Example</h1>
+//       <Profiler id="MyAppProfiler" onRender={onRenderCallback}>
+       
+//         <Count/>
+//       </Profiler>
+//         </div>
+//       );
+// }
+// export default App;
